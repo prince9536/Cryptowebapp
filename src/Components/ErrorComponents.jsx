6158fd1ca9da1954c17007d85a2a47755e9e8830
prince9536 <princebhardwajp48@gmail.com>
2024-04-08 12:26:40 +0530

    Error component updated

--- src/Components/ErrorComponents.jsx
+++ src/Components/ErrorComponents.jsx
@@ -6,10 +6,10 @@ const ErrorComponents = ({ message }) => {
     <Alert
       status="error"
       position={"fixed"}
-      bottom={10} 
+      bottom={4} 
       left={"50%"}
       transform={"translateX(-50%)"}
-      w={"container.lg"}
+      
     >
       <AlertIcon />
       {message}
